5f0bb5425798a42c4c867ce23f62b409b9701649
Nikhil <nikhil.patel6927@gmail.com>
2018-02-10 14:11:21 -0600

    Added some stats and a name checker on player .py

--- player.py
+++ player.py
@@ -1,34 +1,35 @@
 import items
 
+name = input("What is your name kid? ")
+
 class Player:
+	def __str__(self):
+		self.name = name
+		return """Ha, welcome to the Space Training Program. Oh, you forgot your name? 
+It's """ + self.name + """."""
 	def __init__(self):
-		self.inventory = [items.Rock(),
-						items.Dagger(),
-						'Gold(5)',
-						'Crusty Bread']
-						
-		self.x = 1
+		#self.inventory = [items.Rock(),				#EDIT THE INVENTORY AFTER ALL STATS FOR PLAYER ARE ADDED
+						#items.Dagger(),				#EDIT THE INVENTORY AFTER ALL STATS FOR PLAYER ARE ADDED
+						#'Gold(5)',					#EDIT THE INVENTORY AFTER ALL STATS FOR PLAYER ARE ADDED
+						#'Crusty Bread']				#EDIT THE INVENTORY AFTER ALL STATS FOR PLAYER ARE ADDED
+		self.health	= 100
+		self.ammo = 10
+		self.shields = 50
+		self.accuracy = 75			#<40 is garbage,   around 50 is okay,    >60 is great
+		self.damage = 50
+		self.reloadtime = 2
+		self.x = 1   					#will change to start on the ship later
 		self.y = 2
+	def reload(self):
+		if self.ammo == 0:
+			time.sleep(reloadtime)
+			self.ammo = 10
 
 	def print_inventory(self):
 		print("Inventory:")
 		for item in self.inventory:
 			print('* ' + str(item))
-			best_weapon = self.most_powerful_weapon()
-		print("Your best weapon is your {}".format(best_weapon))
-	
-	def most_powerful_weapon(self):
-		max_damage = 0
-		best_weapon = None
-		for item in self.inventory:
-			try:
-				if item.damage > max_damage:
-					best_weapon = item
-					max_damage = item.damage
-			except AttributeError:
-				pass
-		return best_weapon
-		
+			
 	def move(self, dx, dy):
 		self.x += dx
 		self.y += dy
@@ -44,3 +45,10 @@ class Player:
 
 	def move_west(self):
 		self.move(dx=-1, dy=0)
+	def shoot(self):
+		self.ammo = self.ammo - 1
+def myself():
+	print(Player())
+	
+myself()
+
