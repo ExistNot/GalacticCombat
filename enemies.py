5ddcd63053d3660119ffc12f2b5a5ee7058be095
Adi Ojha <adiojha629@gmail.com>
2018-02-17 13:41:42 -0600

    Finsh enemies.py

--- enemies.py
+++ enemies.py
@@ -9,9 +9,36 @@ class Enemy:
 		return self.hp > 0
 
 
-class Invader(Enemy):
+class Invader(Enemy):## ground troop
 	def __init__(self):
 		self.name = "Invader"
 		self.hp = 75
 		self.damage = 15
 		self.accuracy = 35
+	def shoot(self):
+		return random.randint(1,100)<=self.accuracy
+	def hit(self):
+		self.hp = self.hp-1
+		
+class Boss(Enemy):
+        def __init__ (self):
+                self.name = "Boss"
+                self.hp = 10,000
+                self.damage = 250##Subject to change
+                self.accuracy = 100
+        def shoot(self):
+                return self.damage
+class Invader(Enemy):## ground troop
+	def __init__(self):
+		self.name = "Epod"
+		self.hp = 250
+		self.damage = 15
+		self.accuracy = 35
+	def shoot(self):
+		return random.randint(1,100)<=self.accuracy
+	def hit(self):
+		self.hp = self.hp-1
+
+        
+
+        
