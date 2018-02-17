6ad1eaceb0da723ddc7308a2e32ad5062e0fd3b4
Nikhil <nikhil.patel6927@gmail.com>
2018-02-17 12:19:07 -0600

    Pasted code to enemies.py

--- player.py
+++ player.py
@@ -15,8 +15,8 @@ It's """ + self.name + """."""
 		self.accuracy = 75			#<40 is garbage,   around 50 is okay,    >60 is great
 		self.damage = 50
 		self.reloadtime = 2
-		self.x = 1   					#will change to start on the ship later
-		self.y = 2
+		self.x = 1   					
+		self.y = 1
 	def reload(self):
 		if self.ammo == 0:
 			time.sleep(reloadtime)
@@ -49,3 +49,35 @@ def myself():
 	
 myself()
 
+class PlayerShip:
+	def __str__(self):
+		return """Your trusty ship!"""
+	def __init__(self):
+		self.inventory = [items.PBlaster]
+		self.health	= 100
+		self.ammo = 10
+		self.shields = 50
+		self.damage = 50
+		self.x = 1   					#will change to start on the ship later
+		self.y = 1
+	def reload(self):
+		if self.ammo == 0:
+			time.sleep(reloadtime)
+			self.ammo = 10
+	def move(self, dx, dy):
+		self.x += dx
+		self.y += dy
+
+	def move_north(self):
+		self.move(dx=0, dy=-1)
+
+	def move_south(self):
+		self.move(dx=0, dy=1)
+
+	def move_east(self):
+		self.move(dx=1, dy=0)
+
+	def move_west(self):
+		self.move(dx=-1, dy=0)
+	def shoot(self):
+		self.ammo = self.ammo - 1
