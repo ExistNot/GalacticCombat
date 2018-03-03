b16b75b39668030b0874096b7e977c5b9664c735
Adi Ojha <adiojha629@gmail.com>
2018-03-03 07:40:45 -0600

    Added a getinShip function, to be used later

--- player.py
+++ player.py
@@ -17,6 +17,7 @@ It's """ + self.name + """."""
 		self.y = 1
 		self.damage = items.PBlaster.damage
 		self.ship = PlayerShip()
+		self.shipOpen = False
 	def reload(self):
 		if self.ammo == 0:
 			time.sleep(reloadtime)
@@ -70,6 +71,41 @@ It's """ + self.name + """."""
 				self.inventory.pop(index)
 			self.gold += gold_total
 			print("Your wealth increased by %d Gold." % gold_total)
+	def getinShip(self):
+		if(self.shipOpen):
+			return[True,"You have access to the ship"]
+		else:
+			counter = 0 #We will check for the 4 items needed to "enter" the ship
+			hammerIndex = 0
+			nailIndex = 0
+			cortexIndex = 0
+			cannonIndex = 0
+			for index in range(len(self.inventory)):
+				if(inventory[index].name.lower() == 'hammer'):
+					counter+=1
+					hammerIndex = index
+				if(inventory[index].name.lower() == 'nail'):
+					counter+=1
+					nailIndex = index
+				if(inventory[index].name.lower() == 'cortex'):
+					counter+=1
+					cortexIndex = index
+				if(inventory[index].name.lower() == 'fusion cannon'):
+					counter+=1
+					cannonIndex = index
+			if(counter == 4):#All items are present
+				self.inventory.pop(hammerIndex)
+				self.inventory.pop(nailIndex)
+				self.inventory.pop(cortex)
+				self.inventory.pop(cannonIndex)
+				#all items have been removed
+				self.shipOpen = True
+				return [True,"You hammer the nail in place. This cortex fits in smoothly. The fusion cannon is attached behind the ship's barrel. You now have a functioning ship"]
+			else:#help the player realize what they don't have
+				return[False,"You can't access the ship. You need a Fusion Cannon,Cortex,Hammer and Nail"]
+				
+				
+				 
 def myself():
 	print(Player())
 
