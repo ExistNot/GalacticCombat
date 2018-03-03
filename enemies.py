45f3d47cbc896f4a36d1d93d956b25293b5e867a
Dr. Foland <stevenfoland@tsogiants.org>
2018-03-03 15:32:20 -0600

    Updated enemies to match Dr.Foland's version

--- enemies.py
+++ enemies.py
@@ -1,50 +1,71 @@
 class Enemy:
-	def __init__(self):
-		raise NotImplementedError("Do not create raw Enemy objects.")
+	name = "Do not create raw enemies!"
+	description = "There is no description here because you should not create raw Enemy objects!"
+	attack_description = "There is no attack_description here because you should not create raw Enemy objects!"
+	
+	hp = 0
+	damage = 0
+	
+	loot = []
+	
+	agro = False	# Used to cause enemies to attack spontaneously.
+	
+	def __init__(self, direction = None, loot = []):
+		if(direction == 'n'):
+			self.direction = 'north'
+		elif(direction == 's'):
+			self.direction = 'south'
+		elif(direction == 'e'):
+			self.direction = 'east'
+		elif(direction == 'w'):
+			self.direction = 'west'
+		else:
+			self.direction = None
+		
+		if(len(self.loot) > 0):
+			for item in loot:
+				self.loot.append(item)
+		else:
+			self.loot = loot
 
 	def __str__(self):
 		return self.name
+		
+	def check_text(self):
+		text = ""
+		if(self.direction):
+			text = "A %s is blocking your progress to the %s." % (self.name, self.direction)
+		text += " " + self.description			
+		return text
 
+	def take_damage(self, amount):
+		self.hp -= amount
+		if(self.hp <= 0):
+			self.hp = 0
+			defeat_text = "The %s is defeated." % self.name
+			if(len(self.loot) > 0):
+				defeat_text += " It dropped the following items: "
+				for item in self.loot:
+					defeat_text += "* " + str(item)
+			return defeat_text
+		else:
+			return "The %s took %d damage." % (self.name, amount)
+			
 	def is_alive(self):
 		return self.hp > 0
 		
 	def handle_input(self, verb, noun1, noun2, inventory):
 		return [False, None, inventory]
-	
-	def status(self):
-		print("Enemy HP: "+self.hp)
 
 
 class Invader(Enemy):## ground troop
-	def __init__(self):
-		self.name = "Invader"
-		self.hp = 75
-		self.damage = 15
-		self.accuracy = 35
-	def shoot(self):
-		return random.randint(1,100)<=self.accuracy
-	def hit(self):
-		self.hp = self.hp-1
-		
+	name = "Enemy Invader"
+	description = "This is a ruthless killing machine. Kill it before it get you!"
+	hp = 30
+	damage = 10
+
 class Boss(Enemy):
-        def __init__ (self):
-                self.name = "Boss"
-                self.hp = 10,000
-                self.damage = 250##Subject to change
-                self.accuracy = 100
-        def shoot(self):
-                return self.damage
-class Invader(Enemy):## ground troop
-	def __init__(self):
-		self.name = "Epod"
-		self.hp = 250
-		self.damage = 15
-		self.accuracy = 35
-	def shoot(self):
-		return random.randint(1,100)<=self.accuracy
-	def hit(self):
-		self.hp = self.hp-1
-
-        
-
-        
+	name = "Boss Invader"
+	description = "The king of all invaders."
+	hp = 300
+	damage = 100
